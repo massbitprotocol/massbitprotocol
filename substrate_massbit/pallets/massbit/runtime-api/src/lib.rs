576549c839d114ee1b54b5daa81b87aeb2af01af
huy <anhhuy0501@gmail.com>
2021-05-06 18:00:32 +0700

    update get_worker api that output custom type AccountId

--- substrate_massbit/pallets/massbit/runtime-api/src/lib.rs
+++ substrate_massbit/pallets/massbit/runtime-api/src/lib.rs
@@ -2,12 +2,14 @@
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::unnecessary_mut_passed)]
 use sp_std::vec::Vec;
-
+use frame_support::Parameter;
 // Here we declare the runtime API. It is implemented it the `impl` block in
 // runtime amalgamator file (the `runtime/src/lib.rs`)
 sp_api::decl_runtime_apis! {
-	pub trait SumStorageApi {
-		fn get_workers() -> Vec<Vec<u8>>;
+	pub trait SumStorageApi<AccountId> where
+	AccountId: Parameter,
+	{
+		fn get_workers() -> Vec<(u32,Vec<u8>,AccountId,u32)>;
 		fn get_job_reports() -> Vec<(u32,u32,u32)>;
 	}
 }
