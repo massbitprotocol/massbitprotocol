c1f2d4491e868fae8c64d956ff1febfd3df5772b
huy <anhhuy0501@gmail.com>
2021-05-11 14:59:26 +0700

    add helper for submit report

--- worker_agent/provider/client_submit_report.py
+++ worker_agent/provider/client_submit_report.py
@@ -13,10 +13,16 @@ if __name__ == "__main__":
     # result = get_job_result(url,method,block_number)
 
     response = input("Input the response form worker for hashing:")
+    url = input("Input the worker url:")
+    block_number = input("Input the block_number:")
 
     # Hash the output
     hash_response = hash_output(response+"\n")
 
-    print(f"Hash of the response: {hash_response}")
+    job_input = "{\"url\":\"http://" + url + "/\",\"method\":\"eth_getBlockTransactionCountByNumber\",\"block_number\":\"" + block_number + "\",\"job_proposal\":\"0\"}"
+
+    # Print job_input for report
+    print(f"job_input: {job_input}")
+    print(f"job_output: {hash_response}")
 
 
