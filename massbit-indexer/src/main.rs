a7fdfd3fd11f638bc55c0e4876c2756e906b9740
anhhuy <anhhuy0501@gmail.com>
2021-06-25 19:10:33 +0700

    add stream out (not finish)

--- massbit-indexer/src/main.rs
+++ massbit-indexer/src/main.rs
@@ -1,26 +1,88 @@
-/*
-    Copyright 2019 Supercomputing Systems AG
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
-///! Very simple examples that shows how to pretty print the metadata. Has proven to be a helpful
-///! debugging tool.
-
 use massbit_indexer::substrate_chain;
+use tonic::{transport::Server, Request, Response, Status};
+use std::sync::Arc;
+use tokio_stream::wrappers::ReceiverStream;
+use tokio::sync::mpsc;
+
+pub mod stream_mod {
+    tonic::include_proto!("streamout");
+}
+
+use stream_mod::streamout_server::{Streamout, StreamoutServer};
+use stream_mod::{HelloReply, HelloRequest, GenericDataProto};
+
+#[derive(Debug, Default)]
+pub struct StreamService {
+    ls_generic_data: Arc<Vec<GenericDataProto>>
+}
+
+#[tonic::async_trait]
+impl Streamout for StreamService {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name).into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    type ListFeaturesStream = ReceiverStream<Result<GenericDataProto, Status>>;
+
+    async fn list_features(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<Self::ListFeaturesStream>, Status> {
+        println!("ListFeatures = {:?}", request);
+
+        let (tx, rx) = mpsc::channel(4);
+        let mut ls_generic_data = self.ls_generic_data.clone();
+
+        tokio::spawn(async move {
+            loop {
+                if !ls_generic_data.is_empty(){
+                    let generic_data = ls_generic_data.pop();
+                    println!("  => send {:?}", generic_data.unwrap());
+                    tx.send(Ok(generic_data.clone())).await.unwrap();
+                }
+            }
+
+            println!(" /// done sending");
+        });
+
+        Ok(Response::new(ReceiverStream::new(rx)))
+    }
 
 
-fn main() {
+}
+
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "127.0.0.1:50051".parse()?;
+
+    let generic_data = GenericDataProto{
+        version: "1".to_string()
+    };
+    let mut stream_service = StreamService {
+        ls_generic_data: Arc::new(Vec::from([generic_data]))
+    };
+
+    stream_service.ls_generic_data.push(generic_data);
+
+    Server::builder()
+        .add_service(StreamoutServer::new(stream_service))
+        .serve(addr)
+        .await?;
 
     substrate_chain::get_data();
+    Ok(())
+
 }
 
--- massbit-indexer/src/substrate_chain.rs
+++ massbit-indexer/src/substrate_chain.rs
@@ -1,37 +1,73 @@
 use clap::App;
 
-use sp_core::sr25519;
+use sp_core::{sr25519, H256 as Hash};
 
 use node_template_runtime::{Block, Header, SignedBlock};
 use std::sync::mpsc::channel;
 use substrate_api_client::Api;
+use substrate_api_client::rpc::json_req;
 use env_logger;
 use serde_json;
 use serde::{Serialize, Deserialize};
+use std::error::Error;
+use substrate_api_client::utils::FromHexString;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct MyBlock{
-    block: Block
-}
+const CHAIN_TYPE: ChainType = ChainType::Substrate;
+const VERSION:&str = "1";
 
-
+#[derive(Debug)]
 enum ChainType{
     Substrate,
     Ethereum,
+    Solana,
 }
+
+#[derive(Debug)]
 enum DataType{
     Block,
     Event,
     Transaction,
 }
-
+#[derive(Debug)]
 struct GenericData{
     chain_type: ChainType,
-    data_type: DataType,
     version: String,
+    data_type: DataType,
     block_hash: String,
-    time_stamp: String,
+    block_number: u64,
     payload: Vec<u8>
+}
+
+fn create_generic_block_from_header(api:&Api<sr25519::Pair>, header:Header) -> Result<GenericData, Box<dyn Error>> {
+    // Get block number
+    let block_number = header.number;
+    // Get Call rpc to block hash
+    let hash = api.get_request(json_req::chain_get_block_hash(Some(block_number)).to_string())?;
+    let hash = hash.unwrap();
+    let block_hash = Hash::from_hex(hash.clone());
+
+    // Call RPC to get block
+    let block = api.get_block::<Block>(Some(block_hash.unwrap())).unwrap().unwrap();
+    Ok(_create_generic_block(hash,&block))
+
+}
+
+fn _create_generic_block(   block_hash: String,
+                            block:&Block) -> GenericData
+{
+    // Remove exstrinsics because cannot deserialize them. Maybe because of `Extrinsic: MaybeSerialize`
+    // Todo: Deserialize exstrinsic
+    let mut block = (*block).clone();
+    block.extrinsics = Vec::new();
+    let generic_block = GenericData{
+        chain_type: CHAIN_TYPE,
+        version: VERSION.to_string(),
+        data_type: DataType::Block,
+        block_hash: block_hash,
+        block_number: block.header.number as u64,
+        payload: serde_json::to_vec(&block).unwrap()
+    };
+    generic_block
 }
 
 pub fn get_data(){
@@ -58,19 +94,6 @@ pub fn get_data(){
         api.get_header::<Header>(None).unwrap()
     );
 
-    let wrap_block = MyBlock{ block:api.get_block::<Block>(None).unwrap().unwrap()};
-    println!(
-        "Latest block: \n {:?} \n",
-        wrap_block
-    );
-
-
-    let serialized = serde_json::to_string(&wrap_block).unwrap();
-    println!("serialized = {:?}", &serialized);
-
-    // let deserialized: MyBlock = serde_json::from_str(&serialized).unwrap();
-    // println!("deserialized = {:?}", deserialized);
-
     println!("Subscribing to finalized heads");
     let (sender, receiver) = channel();
     api.subscribe_finalized_heads(sender).unwrap();
@@ -80,7 +103,9 @@ pub fn get_data(){
             .recv()
             .map(|header| serde_json::from_str(&header).unwrap())
             .unwrap();
-        println!("Got new Block {:?}", head);
+        println!("Got new header {:?}", head);
+        let generic_block = create_generic_block_from_header(&api, head);
+        println!("Got new header {:?}", generic_block);
     }
 }
 
