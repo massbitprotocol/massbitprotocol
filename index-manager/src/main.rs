661d7c6687198a9c32bf187a81600a3a7ff5ca7c
thala321 <phanthanhhuy1996@gmail.com>
2021-06-28 16:39:21 +0700

    Start a thread everytime there is a request to the index manager

--- index-manager/src/main.rs
+++ index-manager/src/main.rs
@@ -52,13 +52,10 @@ async fn main() {
 fn deploy_handler(
     params: DeployParams,
 ) {
-    println!("{}",params.index_name);
-
-    // Load manifest from IPFS
-
-    // Read manifest
-    // SubgraphManifest::resolve(id, &resolver, &LOGGER)
-    IndexDeployment::load_file(params.config_url);
+    loop {
+        IndexDeployment::load_file(params.config_url.clone()); // We are using loop for the indexing demo, but using clone is not efficient here
+        thread::sleep(Duration::from_secs(1));
+    }
 }
 
 // fn from_manifest(
@@ -132,15 +129,15 @@ impl JsonRpcServer {
         // Reason: https://stackoverflow.com/questions/61292425/how-to-run-an-asynchronous-task-from-a-non-main-thread-in-tokio
         handler.add_method("index_deploy", |params: Params| {
             thread::spawn(|| {
-                println!("Received an index request"); // Refactor to use slog logger
-                let params = params.parse().unwrap(); // Refactor to add param check
+                let params: DeployParams = params.parse().unwrap(); // Refactor to add param check
+                println!("Received an index request from {}", params.index_name); // Refactor to use slog logger
+
                 deploy_handler(params);
             });
-            Ok(Value::String("hello".into()))
+            Ok(Value::String("Index deployed success".into()))
         });
 
         let server = ServerBuilder::new(handler)
-            // .cors(DomainsValidation::AllowOnly(vec![AccessControlAllowOrigin::Null]))
             .start_http(&http_addr.parse().unwrap())
             .expect("Unable to start RPC server");
 
@@ -227,7 +224,7 @@ impl IndexDeployment {
     //         .map_err(SubgraphManifestResolveError::ResolveError)
     // }
 
-    // Hughie: Lazily read config from local file
+    // Hughie: Lazily read config from a local file
     fn load_file(
         config_url: String,
     ) {
@@ -238,12 +235,12 @@ impl IndexDeployment {
             .as_str()
             .map(|s| s.to_string())
             .ok_or(anyhow!("Could not find schema file"));
-        println!("Schema: {}",schemaFile.unwrap());
+        println!("Schema: {}",schemaFile.unwrap()); // Refactor to use slog logge
 
         let kind = data["dataSources"][0]["kind"]
             .as_str()
             .map(|s| s.to_string())
             .ok_or(anyhow!("Could not find network kind"));
-        println!("Kind: {}",kind.unwrap());
+        println!("Kind: {}",kind.unwrap()); // Refactor to use slog logge
     }
 }
