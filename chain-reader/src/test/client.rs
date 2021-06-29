a4e82cfbfea63d79c227acd8b262985a95ecdc0b
anhhuy <anhhuy0501@gmail.com>
2021-06-29 17:27:52 +0700

    add code example for get grpc + update proto file

--- chain-reader/src/test/client.rs
+++ chain-reader/src/test/client.rs
@@ -1,18 +1,39 @@
 #[allow(unused_imports)]
-use tonic::{transport::Server, Request, Response, Status};
-use crate::stream_mod::{HelloReply, HelloRequest, GenericDataProto};
+use tonic::{transport::{Server, Channel}, Request, Response, Status};
+use crate::stream_mod::{HelloReply, HelloRequest, GetBlocksRequest, GenericDataProto};
 use stream_mod::streamout_client::{StreamoutClient};
+use std::error::Error;
 
 pub mod stream_mod {
     tonic::include_proto!("streamout");
 }
 
-const URL: &str = "127.0.0.1:50051";
+const URL: &str = "http://127.0.0.1:50051";
+
+async fn print_blocks(client: &mut StreamoutClient<Channel>) -> Result<(), Box<dyn Error>> {
+    // Not use start_block_number start_block_number yet
+    let get_blocks_request = GetBlocksRequest{
+        start_block_number: 0,
+        end_block_number: 1,
+    };
+    let mut stream = client
+        .list_blocks(Request::new(get_blocks_request))
+        .await?
+        .into_inner();
+
+    while let Some(block) = stream.message().await? {
+        let block = block as GenericDataProto;
+        println!("Recieved block = {:?}, hash = {:?}",block.block_number, block.block_hash);
+    }
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = StreamoutClient::connect(URL).await?;
 
+    // Ping server
     println!("*** SIMPLE RPC ***");
     let response = client
         .say_hello(Request::new(HelloRequest {
@@ -22,14 +43,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("RESPONSE = {:?}", response);
 
-    // println!("\n*** SERVER STREAMING ***");
-    // print_features(&mut client).await?;
-    //
-    // println!("\n*** CLIENT STREAMING ***");
-    // run_record_route(&mut client).await?;
-    //
-    // println!("\n*** BIDIRECTIONAL STREAMING ***");
-    // run_route_chat(&mut client).await?;
+    print_blocks(&mut client).await;
 
     Ok(())
 }
