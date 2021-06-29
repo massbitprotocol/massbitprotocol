40fad6d48255476a9036b8de4bc6d70d33214c87
thala321 <phanthanhhuy1996@gmail.com>
2021-06-29 14:38:49 +0700

    Use env_loggger & log so we don't have to pass logger into every functions and struct we're using (unlike the-graph)

--- indexer/index-manager/src/main.rs
+++ indexer/index-manager/src/main.rs
@@ -1,9 +1,14 @@
 // Massbit dependencies
 use json_rpc_server::core::JsonRpcServer;
 use ipfs_client::core::create_ipfs_clients;
+use logger::core::init_logger;
 
 #[tokio::main]
 async fn main() {
+    // Logger
+    init_logger();
+    log::info!("Application started");
+
     // Start JSON RPC Server
     let server = JsonRpcServer::serve(
         "127.0.0.1:3030".to_string(),
@@ -14,4 +19,6 @@ async fn main() {
     let ipfs_addresses = vec!["0.0.0.0:5001".to_string()];
     create_ipfs_clients(&ipfs_addresses).await; // This doesn't wait for connection
 }
+
+
 
