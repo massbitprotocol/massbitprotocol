07316249db46a7383044b6f3f81c1ece68aa6763
thala321 <phanthanhhuy1996@gmail.com>
2021-07-01 09:19:58 +0700

    Remove tokio spawn in ipfs client and add function to get bytes from IPFS hash

--- indexer/index-manager/src/main.rs
+++ indexer/index-manager/src/main.rs
@@ -1,3 +1,4 @@
+use tokio_compat_02::FutureExt;
 // Massbit dependencies
 use index_manager_server::core::JsonRpcServer;
 use ipfs_client::core::create_ipfs_clients;
@@ -10,8 +11,17 @@ async fn main() {
     log::info!("[Indexer Manager] Application started");
 
     // Start IPFS Clients
-    // let ipfs_addresses = vec!["0.0.0.0:5001".to_string()];
-    // create_ipfs_clients(&ipfs_addresses).await;
+    // Use lazy_static to put this ipfs_clients into global envs
+    let ipfs_addresses = vec!["0.0.0.0:5001".to_string()];
+    let ipfs_clients = create_ipfs_clients(&ipfs_addresses).await;
+    let file_bytes = ipfs_clients[0]
+        .cat_all("QmZcB5qty5vGFRw2pEmwmyuyLxNbHM2dBaenNWj64JZ8uo".to_string())
+        .compat()
+        .await
+        .unwrap()
+        .to_vec();
+
+    let raw: serde_yaml::Mapping = serde_yaml::from_slice(&file_bytes).unwrap();
 
     // Start JSON RPC Server
     let server = JsonRpcServer::serve(
