0581d0f38f894892ec4b023faa1705646d7a31f1
datzero9 <nguyenmanhdat2903@gmail.com>
2021-07-07 18:25:56 +0700

    Move Store from dependency to global in plugin

--- plugin-test/src/mapping.rs
+++ plugin-test/src/mapping.rs
@@ -1,23 +1,23 @@
 use crate::models::BlockTs;
-
+use crate::STORE;
 use massbit_chain_substrate::data_type::SubstrateBlock;
 use plugin::core::BlockHandler;
-use store::Store;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Indexer;
 
 impl BlockHandler for Indexer {
-    fn handle_block(
-        &self,
-        store: &mut dyn Store,
-        block: &SubstrateBlock,
-    ) -> Result<(), Box<dyn std::error::Error>> {
+    fn handle_block(&self, block: &SubstrateBlock) -> Result<(), Box<dyn std::error::Error>> {
         let block_ts = BlockTs {
             block_hash: block.header.hash().to_string(),
             block_height: block.header.number as i64,
         };
-        store.save("blocks".to_string(), block_ts.into());
+        unsafe {
+            STORE
+                .as_ref()
+                .unwrap()
+                .save("blocks".to_string(), block_ts.into());
+        }
         Ok(())
     }
 }
