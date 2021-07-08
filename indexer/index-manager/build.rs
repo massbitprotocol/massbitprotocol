3d09cb975252a1cc6f8d773cf77d6fde142cf244
anhhuy <anhhuy0501@gmail.com>
2021-07-08 17:31:49 +0700

    rename massbit-core -> core

--- indexer/index-manager/build.rs
+++ indexer/index-manager/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../massbit-core/proto/chaindata.proto")?;
+    tonic_build::compile_protos("../../core/proto/chaindata.proto")?;
     Ok(())
 }
