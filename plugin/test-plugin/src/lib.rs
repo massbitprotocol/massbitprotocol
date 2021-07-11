70fcd02eaa5bec3818df03f82c451c912ca6efc0
datzero9 <nguyenmanhdat2903@gmail.com>
2021-07-11 14:08:24 +0700

    Move plugin declaration to lib, clean up user-defined mapping

--- plugin/test-plugin/src/lib.rs
+++ plugin/test-plugin/src/lib.rs
@@ -1,16 +1,26 @@
 mod mapping;
 mod models;
 
-use plugin::core::PluginRegistrar;
+use massbit_chain_substrate::data_type::SubstrateBlock;
+use plugin::core::{BlockHandler, PluginRegistrar};
 use store::Store;
 
 #[doc(hidden)]
 #[no_mangle]
 pub static mut STORE: Option<&dyn Store> = None;
 
+plugin::export_plugin!(register);
+
 #[allow(dead_code, improper_ctypes_definitions)]
 extern "C" fn register(registrar: &mut dyn PluginRegistrar) {
-    registrar.register_block_handler("test", Box::new(mapping::Indexer));
+    registrar.register_block_handler("test", Box::new(Indexer));
 }
 
-plugin::export_plugin!(register);
+#[derive(Debug, Clone, PartialEq)]
+pub struct Indexer;
+
+impl BlockHandler for Indexer {
+    fn handle_block(&self, block: &SubstrateBlock) -> Result<(), Box<dyn std::error::Error>> {
+        mapping::handle_block(block)
+    }
+}
