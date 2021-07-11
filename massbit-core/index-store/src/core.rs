a61a79bd011a980d9f536b76b6e01e47d1701ee1
thala321 <phanthanhhuy1996@gmail.com>
2021-07-11 17:36:19 +0700

    Note on what migrating mock store to index store

--- massbit-core/index-store/src/core.rs
+++ massbit-core/index-store/src/core.rs
@@ -7,17 +7,5 @@ pub struct IndexStore {
 }
 
 impl IndexStore {
-    pub fn save<T, M>(&self, table: T, records: M)
-        where
-            T: Table,
-            M: Insertable<T>,
-            InsertStatement<T, M::Values>: ExecuteDsl<PgConnection>,
-    {
-        println!("[Index Store] Writing to database");
-        let connection = PgConnection::establish(&self.connection_string).expect(&format!("Error connecting to {}", self.connection_string));
-
-        let _ = diesel::insert_into(table)
-            .values(records)
-            .execute(&connection);
-    }
+    // Move MockStore of Đạt here
 }
