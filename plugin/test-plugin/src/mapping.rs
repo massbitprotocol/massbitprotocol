a686f33617297e768cf440c105180c29b392fc57
datzero9 <nguyenmanhdat2903@gmail.com>
2021-07-11 14:08:24 +0700

    Move plugin declaration to lib, clean up user-defined mapping

--- plugin/test-plugin/src/mapping.rs
+++ plugin/test-plugin/src/mapping.rs
@@ -1,18 +1,11 @@
 use crate::models::BlockTs;
-
 use massbit_chain_substrate::data_type::SubstrateBlock;
-use plugin::core::BlockHandler;
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Indexer;
-
-impl BlockHandler for Indexer {
-    fn handle_block(&self, block: &SubstrateBlock) -> Result<(), Box<dyn std::error::Error>> {
-        let block_ts = BlockTs {
-            block_hash: block.header.hash().to_string(),
-            block_height: block.header.number as i64,
-        };
-        block_ts.save();
-        Ok(())
-    }
+pub fn handle_block(block: &SubstrateBlock) -> Result<(), Box<dyn std::error::Error>> {
+    let block_ts = BlockTs {
+        block_hash: block.header.hash().to_string(),
+        block_height: block.header.number as i64,
+    };
+    block_ts.save();
+    Ok(())
 }
