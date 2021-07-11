0c1402f7d721a776f912a96e07f7e8ff123228e1
datzero9 <nguyenmanhdat2903@gmail.com>
2021-07-11 22:48:41 +0700

    Generate mapping from manifest file

--- cli/src/codegen.rs
+++ cli/src/codegen.rs
@@ -6,10 +6,12 @@ use serde::Serialize;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
+use std::fs::File;
 use tera::{Context, Tera};
 
 pub fn run(_matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
     generate_rust_entity()?;
+    generate_plugin()?;
     Ok(())
 }
 
@@ -33,9 +35,37 @@ fn generate_rust_entity() -> Result<(), Box<dyn Error>> {
     }
 
     let mut tera = Tera::default();
-    tera.add_raw_template("models", include_str!("templates/models.rs"))?;
+    tera.add_raw_template("models", include_str!("templates/models.rs.tmpl"))?;
     let data = tera.render("models", &Context::from_serialize(binding)?)?;
     fs::write("src/models.rs", data)?;
+
+    Ok(())
+}
+
+#[derive(Serialize)]
+pub struct HandlerBinding {
+    pub handlers: HashMap<String, String>,
+}
+
+fn generate_plugin() -> Result<(), Box<dyn Error>> {
+    let f = File::open("project.yaml")?;
+    let manifest: serde_yaml::Value = serde_yaml::from_reader(f)?;
+    let handlers = manifest["dataSources"][0]["mapping"]["handlers"]
+        .as_sequence()
+        .unwrap();
+    let mut binding = HandlerBinding {
+        handlers: HashMap::new(),
+    };
+    for (_, handler) in handlers.iter().enumerate() {
+        let name = handler["handler"].as_str().map(|s| s.to_string()).unwrap();
+        let kind = handler["kind"].as_str().map(|s| s.to_string()).unwrap();
+        binding.handlers.insert(name, kind);
+    }
+
+    let mut tera = Tera::default();
+    tera.add_raw_template("plugin", include_str!("templates/lib.rs.tmpl"))?;
+    let data = tera.render("plugin", &Context::from_serialize(binding)?)?;
+    fs::write("src/lib.rs", data)?;
 
     Ok(())
 }
