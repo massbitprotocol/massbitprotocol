4851e03730bee28e46e9b730254cc554535ca492
thala321 <phanthanhhuy1996@gmail.com>
2021-07-12 15:43:17 +0700

    Migrating plugin manager v2 to use our index-store

--- core/index-store/src/core.rs
+++ core/index-store/src/core.rs
@@ -1,24 +1,43 @@
-use diesel::{Insertable, Table, PgConnection, RunQueryDsl, Connection};
-use diesel::query_builder::InsertStatement;
-use diesel::query_dsl::load_dsl::ExecuteDsl;
+use diesel::{PgConnection, RunQueryDsl, Connection};
+use structmap::GenericMap;
 
 pub struct IndexStore {
     pub connection_string: String,
 }
 
-// Refactor and use the logic from Mockstore here so we are not dependant on diesel https://github.com/massbitprotocol/massbitprotocol/pull/34/files#diff-cb28164240a17b00fd8ae0f15957d3159cad203dae86739b642a471c2d1931a0
-impl IndexStore {
-    pub fn save<T, M>(&self, table: T, records: M)
-        where
-            T: Table,
-            M: Insertable<T>,
-            InsertStatement<T, M::Values>: ExecuteDsl<PgConnection>,
-    {
-        println!("[Index Store] Writing to database");
-        let connection = PgConnection::establish(&self.connection_string).expect(&format!("Error connecting to {}", self.connection_string));
+impl Store for IndexStore {
+    fn save(&self, _entity_name: String, mut _data: GenericMap) {
+        let mut query = format!("INSERT INTO {} (", _entity_name);
 
-        let _ = diesel::insert_into(table)
-            .values(records)
-            .execute(&connection);
+        // Compiling the attributes for the insert query
+        // Example: INSERT INTO BlockTs (block_hash,block_height)
+        for (k, _) in &_data {
+            query = format!("{}{},",query, k)
+        }
+        query = query[0..query.len() - 1].to_string(); // Remove the final `,`
+        query = format!("{})",query); // Close the list of attributes
+
+        // Compiling the values for the insert query
+        // Example: INSERT INTO BlockTs (block_hash,block_height) VALUES ('0x720c…6c50',610)
+        query = format!("{} VALUES (",query); // Add the first `(` for the list of attributes
+        for (k, v) in &_data {
+            match v.string() {
+                Some(r) => {
+                    query = format!("{}'{}',",query, r)
+                }
+                _ => {}
+            }
+            match v.i64() {
+                Some(r) => {
+                    query = format!("{}{},",query, r);
+                }
+                _ => {}
+            }
+        }
+        query = query[0..query.len() - 1].to_string(); // Remove the final `,`
+        query = format!("{})",query); // Close the list of attributes
+        println!("{}", query); // Inserting the values into the index table
+        let c = PgConnection::establish(&self.connection_string).expect(&format!("Error connecting to {}", self.connection_string));
+        diesel::sql_query(query).execute(&c);
     }
 }
