fef59791526456aefd5af9a7ea4b75908c1c6a26
thala321 <phanthanhhuy1996@gmail.com>
2021-07-12 17:49:37 +0700

    Add new library implementation so mapping file

--- code-compiler/stub/src/lib.rs
+++ code-compiler/stub/src/lib.rs
@@ -1,15 +1,26 @@
-#[macro_use]
-extern crate diesel;
+mod mapping;
+mod models;
 
-pub mod mapping;
-pub mod models;
-pub mod schema;
+use massbit_chain_substrate::data_type::SubstrateBlock;
+use plugin::core::{BlockHandler, PluginRegistrar};
+use index_store::core::Store;
 
-use plugin::core::PluginRegistrar;
+#[doc(hidden)]
+#[no_mangle]
+pub static mut STORE: Option<&dyn Store> = None;
 
 plugin::export_plugin!(register);
 
-#[allow(dead_code)]
+#[allow(dead_code, improper_ctypes_definitions)]
 extern "C" fn register(registrar: &mut dyn PluginRegistrar) {
-    registrar.register_block_handler("handle_block", Box::new(mapping::BlockIndexer));
+    registrar.register_block_handler("test", Box::new(Indexer));
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Indexer;
+
+impl BlockHandler for Indexer {
+    fn handle_block(&self, block: &SubstrateBlock) -> Result<(), Box<dyn std::error::Error>> {
+        mapping::handle_block(block)
+    }
 }
