ae5810138052855929072d9ca156f7aadf7b144c
anhhuy <anhhuy0501@gmail.com>
2021-07-15 09:32:49 +0700

    update data type

--- core/chain/substrate/src/data_type.rs
+++ core/chain/substrate/src/data_type.rs
@@ -1,8 +1,9 @@
 use node_template_runtime;
 use std::error::Error;
-use sp_runtime::traits::SignedExtension;
+use sp_runtime::traits::{SignedExtension, Extrinsic as _};
 use serde::{Deserialize, Serialize};
 use codec::{Encode, Decode, Input, WrapperTypeDecode};
+
 
 //********************** SUBSTRATE ********************************
 // Main data type for substrate indexing
@@ -15,6 +16,7 @@ type Date = u16;
 type Event = system::EventRecord<node_template_runtime::Event, node_template_runtime::Hash>;
 type Extrinsic = node_template_runtime::UncheckedExtrinsic;
 type Block = node_template_runtime::Block;
+type Hash = node_template_runtime::Hash;
 
 // Similar to
 // https://github.com/subquery/subql/blob/93afc96d7ee0ff56d4dd62d8a145088f5bb5e3ec/packages/types/src/interfaces.ts#L18
@@ -48,12 +50,26 @@ pub type SubstrateHeader = node_template_runtime::Header;
 pub type SubstrateCheckedExtrinsic = node_template_runtime::CheckedExtrinsic;
 pub type SubstrateSignedBlock = node_template_runtime::SignedBlock;
 
+pub trait ExtrinsicTrait {
+    fn is_signed(&self) -> bool;
+    fn get_hash(&self) -> Hash;
+}
+
+impl ExtrinsicTrait for ExtExtrinsic {
+    fn is_signed(&self) -> bool {
+        self.extrinsic.is_signed();
+    }
+    fn get_hash(&self) -> Hash {
+        self.extrinsic.get_hash();
+    }
+}
 
 pub fn get_extrinsics_from_block (block: &ExtBlock) -> Vec<ExtExtrinsic> {
 
     let iter = block.block.extrinsics.iter();
     let extrinsics = iter
         .map(|extrinsic|{
+            let hash = extrinsic.get_hash();
             ExtExtrinsic {
                 block_number: block.block.header.number,
                 extrinsic: (*extrinsic).clone(),
@@ -68,6 +84,7 @@ pub fn get_extrinsics_from_block (block: &ExtBlock) -> Vec<ExtExtrinsic> {
         .collect();
     extrinsics
 }
+
 
 pub fn decode<T>(payload: &mut Vec<u8>) -> Result<T, Box<dyn Error>>
     where T: Decode,
