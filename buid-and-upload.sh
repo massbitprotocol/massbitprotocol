9896a0d3ab7c1d881c220b23423875cde676ac63
thala321 <phanthanhhuy1996@gmail.com>
2021-07-20 11:13:02 +0700

    Upgrade to version 2 and add some logging

--- buid-and-upload.sh
+++ buid-and-upload.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
-docker build --tag sprise/substrate-node:0.1 -f substrate-node/Dockerfile -t substrate-node .;
-docker build --tag sprise/chain-reader:0.1 -f chain-reader/Dockerfile -t chain-reader .;
-docker build --tag sprise/indexer:0.1 -f indexer/Dockerfile -t indexer .;
-docker build --tag sprise/code-compiler:0.1 -f code-compiler/Dockerfile -t code-compiler .;
-docker build --tag sprise/dashboard:0.1 -f frontend/dashboard/Dockerfile -t dashboard . --no-cache; # We need no-cache because we're pulling from github master, or it won't update our new code
+docker build --tag sprise/substrate-node:0.1 -f substrate-node/Dockerfile -t substrate-node .; # We don't need to upgrade this
+docker build --tag sprise/chain-reader:0.2 -f chain-reader/Dockerfile -t chain-reader .;
+docker build --tag sprise/indexer:0.2 -f indexer/Dockerfile -t indexer .;
+docker build --tag sprise/code-compiler:0.2 -f code-compiler/Dockerfile -t code-compiler .;
+docker build --tag sprise/dashboard:0.2 -f frontend/dashboard/Dockerfile -t dashboard . --no-cache; # We need no-cache because we're pulling from github master, or it won't update our new code
 
 docker push sprise/substrate-node:0.1;
-docker push sprise/chain-reader:0.1;
-docker push sprise/indexer:0.1;
-docker push sprise/code-compiler:0.1;
-docker push sprise/dashboard:0.1;
+docker push sprise/chain-reader:0.2;
+docker push sprise/indexer:0.2;
+docker push sprise/code-compiler:0.2;
+docker push sprise/dashboard:0.2;
