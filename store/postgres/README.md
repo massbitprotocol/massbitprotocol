2476af5c4951de88376d456aea06c6cb9c64b44b
viettai <vuviettai@gmail.com>
2021-07-22 17:54:15 +0700

    Change ddlgen from lib to cli to resolve crate conflict

--- store/postgres/README.md
+++ store/postgres/README.md
@@ -2,33 +2,110 @@
 Clone from https://github.com/graphprotocol/graph-node/tree/v0.22.0/store/postgres
 
 Modify file relational.rs to generate ddl as list of seperated queries and list table names
-### Integration
-Include protection against stack overflow when parsing from this PR: 
+# DDL gen CLI
 
-https://github.com/graphql-rust/graphql-parser/commit/45167b53e9533c331298683577ba8df7e43480ac
+## Usage
+```shell
+cargo run -- ddlgen --schema schema.graphql --config project.yaml --ouput migrations 
+```
 
-Add patch to workspace toml
-```
-[patch.crates-io]
-graphql-parser = {git="https://github.com/graphql-rust/graphql-parser", rev="45167b53e9533c331298683577ba8df7e43480ac"}
-```
-Using
-```
-use store_postgres::layout;
-..........................
-    let schema : &str = r#"..."#
-    let namespace = "01";
-    let result = layout::gen_ddls(schema, namespace);
-    match result {
-        Ok(res) => {
-            for ddl in &res.0 {
-                println!("{}", ddl);
+## Input templates
+`schema.graphql`
+```graphql
+   type _Schema_ @fulltext(
+        name: "userSearch"
+        language: en
+        algorithm: rank
+        include: [
+            {
+                entity: "User",
+                fields: [
+                    { name: "name"},
+                    { name: "email"},
+                ]
             }
-            println!("Table names:");
-            for table in &res.1 {
-                println!("{},", table);
+        ]
+    ) @fulltext(
+        name: "nullableStringsSearch"
+        language: en
+        algorithm: rank
+        include: [
+            {
+                entity: "NullableStrings",
+                fields: [
+                    { name: "name"},
+                    { name: "description"},
+                    { name: "test"},
+                ]
             }
-        },
-        Err(_) => println!("Invalid schema")
+        ]
+    )
+
+    type Thing @entity {
+        id: ID!
+        bigThing: Thing!
     }
+
+    enum Color { yellow, red, BLUE }
+
+    type Scalar @entity {
+        id: ID,
+        bool: Boolean,
+        int: Int,
+        bigDecimal: BigDecimal,
+        bigDecimalArray: [BigDecimal!]!
+        string: String,
+        strings: [String!],
+        bytes: Bytes,
+        byteArray: [Bytes!],
+        bigInt: BigInt,
+        bigIntArray: [BigInt!]!
+        color: Color,
+    }
+
+    interface Pet {
+        id: ID!,
+        name: String!
+    }
+
+    type Cat implements Pet @entity {
+        id: ID!,
+        name: String!
+    }
+
+    type Dog implements Pet @entity {
+        id: ID!,
+        name: String!
+    }
+
+    type Ferret implements Pet @entity {
+        id: ID!,
+        name: String!
+    }
+
+    type User @entity {
+        id: ID!,
+        name: String!,
+        bin_name: Bytes!,
+        email: String!,
+        age: Int!,
+        seconds_age: BigInt!,
+        weight: BigDecimal!,
+        coffee: Boolean!,
+        favorite_color: Color,
+        drinks: [String!]
+    }
+
+    type NullableStrings @entity {
+        id: ID!,
+        name: String,
+        description: String,
+        test: String
+    }
+```
+`project.yaml`
+```yaml
+
+database:
+  catalog: dbcatalog
 ```
--- store/postgres/src/ddlgen.rs
+++ store/postgres/src/ddlgen.rs
@@ -1,11 +1,75 @@
 use std::error::Error;
-use graph::prelude::{Schema, SubgraphDeploymentId};
-//use graph_store_postgres::command_support::{Namespace, Catalog, Layout};
-use super::{catalog::{Catalog}, primary::Namespace, relational::{Layout} };
-use lazy_static::lazy_static;
+use std::sync::Arc;
+use std::convert::{From, TryFrom, TryInto};
+use std::fs;
+use std::str::FromStr;
+use graph::prelude::{q, s, Schema, SubgraphDeploymentId, StoreError, ValueType};
+use graph::data::{
+    graphql::ext::{DocumentExt, ObjectTypeExt},
+    subgraph::schema::MetadataType,
+};
+use graph::data::schema::{FulltextConfig, FulltextDefinition, SCHEMA_TYPE_NAME};
 
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
+//use graph_store_postgres::command_support::{Namespace, Catalog, Layout};
+//use store_postgres::{catalog::{Catalog}, primary::Namespace, relational::{Layout} };
+//use super::command_support::{Layout, Namespace, Catalog};
+use lazy_static::lazy_static;
+use clap::ArgMatches;
+use serde_yaml::{Value, Mapping};
+use crate::relational::{Layout, SqlName, PRIMARY_KEY_COLUMN};
+use crate::primary::Namespace;
+use crate::catalog::Catalog;
 lazy_static! {
     static ref THINGS_SUBGRAPH_ID: SubgraphDeploymentId = SubgraphDeploymentId::new("subgraphId").unwrap();
+}
+/*
+/// The SQL type to use for GraphQL ID properties. We support
+/// strings and byte arrays
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub(crate) enum IdType {
+    String,
+    Bytes,
+}
+impl TryFrom<&s::ObjectType> for IdType {
+    type Error = StoreError;
+
+    fn try_from(obj_type: &s::ObjectType) -> Result<Self, Self::Error> {
+        let pk = obj_type
+            .field(&PRIMARY_KEY_COLUMN.to_owned())
+            .expect("Each ObjectType has an `id` field");
+        Self::try_from(&pk.field_type)
+    }
+}
+
+impl TryFrom<&s::Type> for IdType {
+    type Error = StoreError;
+
+    fn try_from(field_type: &s::Type) -> Result<Self, Self::Error> {
+        let name = named_type(field_type);
+
+        match ValueType::from_str(name)? {
+            ValueType::String => Ok(IdType::String),
+            ValueType::Bytes => Ok(IdType::Bytes),
+            _ => Err(anyhow!(
+                "The `id` field has type `{}` but only `String`, `Bytes`, and `ID` are allowed",
+                &name
+            )
+                .into()),
+        }
+    }
+}
+
+type IdTypeMap = HashMap<String, IdType>;
+type EnumMap = BTreeMap<String, Arc<BTreeSet<String>>>;
+*/
+pub fn run(matches: &ArgMatches, dbconfig : &Value) -> Result<(), Box<dyn Error>> {
+    let schema_path = matches.value_of("schema").unwrap_or("schema.graphql");
+    let output = matches.value_of("output").unwrap_or("migrations");
+    let def_cat = Value::String(String::from("subgraph"));
+    let catalog = dbconfig.get("catalog").unwrap_or(&def_cat).as_str().unwrap();
+    let raw_schema = fs::read_to_string(schema_path)?;
+    generate_ddl(raw_schema.as_str(), catalog,output)
 }
 
 ///
@@ -13,23 +77,22 @@ lazy_static! {
 /// Input: graphql schema, namespace in database
 /// Output: Vector of pure queries
 ///
-pub fn gen_ddls(raw: &str, namespace: &str) -> Result<(Vec<String>, Vec<String>), Box<dyn Error>> {
 
+pub fn generate_ddl(raw: &str, catalog: &str, output_dir: &str) -> Result<(), Box<dyn Error>> {
+    //let mut ddls : Vec<String> = Vec::new();
+    //let mut table_names : Vec<String> = Vec::new();
     let schema = Schema::parse(raw, THINGS_SUBGRAPH_ID.clone())?;
-    let mut ns = String::from(namespace);
-    if !ns.starts_with("sgd") || ns.len() <= 3 {
-        ns.insert_str(0, "sgd");
-    }
-    let catalog = Catalog::make_empty(Namespace::new(ns)?)?;
+    let catalog = Catalog::make_empty(Namespace::new(String::from(catalog))?)?;
     match Layout::new(&schema, catalog, false) {
         Ok(layout) => {
             let result = layout.as_ddls()?;
-            Ok(result)
+            fs::write(format!("{}/up.sql", output_dir), result.0.join(";"))?;
+            fs::write(format!("{}/tables.txt", output_dir), result.1.join(";"))?;
+            Ok(())
         },
         Err(_err) => Err(format!("Invalid schema").into())
     }
 }
-
 pub fn gen_ddl(raw: &str, namespace: &str) -> Result<String, Box<dyn Error>> {
 
     let schema = Schema::parse(raw, THINGS_SUBGRAPH_ID.clone())?;
