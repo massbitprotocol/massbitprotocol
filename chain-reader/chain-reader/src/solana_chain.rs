8b81433d160cdc1cb2b3a0dc2bd616a3ff01c220
viettai <vuviettai@gmail.com>
2021-07-30 17:33:41 +0700

    Fix Solana chain reader

--- chain-reader/chain-reader/src/solana_chain.rs
+++ chain-reader/chain-reader/src/solana_chain.rs
@@ -8,13 +8,14 @@ use std::{
         atomic::{AtomicBool, Ordering},
         Arc,
     },
-    time::{Instant}
+    time::{Duration, Instant},
+    thread
 };
 use std::error::Error;
 use massbit_chain_solana::data_type::{  SolanaEncodedBlock as Block,
                                         get_list_log_messages_from_encoded_block,
                                         };
-
+use std::sync::Mutex;
 
 
 // Check https://github.com/tokio-rs/prost for enum converting in rust protobuf
@@ -34,6 +35,77 @@ fn fix_one_thread_not_receive(chan: &broadcast::Sender<GenericDataProto>){
 }
 
 pub async fn loop_get_block(chan: broadcast::Sender<GenericDataProto>) {
+    info!("Start get block Solana");
+    let config = CONFIG.chains.get(&CHAIN_TYPE).unwrap();
+    let json_rpc_url = config.url.clone();
+    let websocket_url = config.ws.clone();
+    info!("Init Solana client");
+    {
+        let (mut subscription_client, receiver) =
+            PubsubClient::slot_subscribe(&websocket_url).unwrap();
+        info!("Finished init Solana client");
+
+        let main_latest_root = Arc::new(Mutex::new(0_u64));
+        let receiver_latest_root = Arc::clone(&main_latest_root);
+        //Listen for incomming message
+        let thread_receiver = tokio::spawn(async move {
+            let start = Instant::now();
+            let mut startblock = 0_u64;
+            loop {
+                if let Ok(slot) = receiver.recv() {
+                    let mut root = receiver_latest_root.lock().unwrap();
+                    let mut rate = 0_f32;
+                    if startblock == 0 {
+                        startblock = slot.root;
+                    } else {
+                        rate = (slot.root - startblock) as f32/ start.elapsed().as_secs_f32();
+                    }
+                    if slot.root > *root + BLOCK_AVAILABLE_MARGIN {
+                        info!("[RECEIVER] Received from websocket root block: {} at rate {} blocks/s", slot.root, rate);
+                        *root = slot.root - BLOCK_AVAILABLE_MARGIN;
+                    }
+                }
+            }
+        });
+        let exit = Arc::new(AtomicBool::new(false));
+        let client = Arc::new(RpcClient::new(json_rpc_url.clone()));
+        let mut last_root: u64 = 0;
+        fix_one_thread_not_receive(&chan);
+        loop {
+            if exit.load(Ordering::Relaxed) {
+                eprintln!("{}", "exit".to_string());
+                subscription_client.shutdown().unwrap();
+                break;
+            }
+            let latest_root = *main_latest_root.lock().unwrap();
+            if latest_root == 0 || latest_root == last_root {
+                continue;
+            } else if last_root == 0 {
+                last_root = latest_root;
+            } else {
+                //0 < last_root < latest_root
+                info!("[MAIN] Latest stable block: {}, Pending block: {}", latest_root, latest_root - last_root);
+                if let Ok(_block_height) = client.get_block_height() {
+                    for block_height in last_root..latest_root {
+                        let new_client = client.clone();
+                        let chan_clone = chan.clone();
+                        tokio::spawn(async move {
+                            if let Ok(block) = get_block(new_client, block_height) {
+                                let generic_data_proto = _create_generic_block(block.block.blockhash.clone(), block_height, &block);
+                                info!("[CHILD] Sending generic data: {:?} of SOLANA  block: {}", &generic_data_proto.block_number, block_height);
+                                chan_clone.send(generic_data_proto).unwrap();
+                            }
+                        });
+                    }
+                    last_root = latest_root;
+                }
+            }
+        }
+    }
+}
+
+
+pub async fn loop_get_block_old(chan: broadcast::Sender<GenericDataProto>) {
     info!("Start get block Solana");
     let config = CONFIG.chains.get(&CHAIN_TYPE).unwrap();
     let json_rpc_url = config.url.clone();
