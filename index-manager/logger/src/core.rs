9abf3cd0f451dc02896bb044206ef99f6fc21b8e
thala321 <phanthanhhuy1996@gmail.com>
2021-08-11 15:53:14 +0700

    Setup log4rs to write logs to file with the name of the log as current timestamp

--- index-manager/logger/src/core.rs
+++ index-manager/logger/src/core.rs
@@ -1,11 +1,14 @@
 use chrono::Local;
 use env_logger::Builder;
 use log::LevelFilter;
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Config, Root};
+use log4rs::encode::pattern::PatternEncoder;
+
 /**
-*** The objective of this file is to parse the env logger to the correct format
+*** The objective of this file is to setup logger writing to file
 **/
 use std::io::Write;
-// This would add timestamp to every logger
 pub fn init_logger() {
     Builder::new()
         .format(|buf, record| {
@@ -17,6 +20,17 @@ pub fn init_logger() {
                 record.args()
             )
         })
-        .filter(None, LevelFilter::Info)
-        .init();
+        .filter(None, LevelFilter::Info);
+    // .init();
+
+    let date = chrono::Utc::now();
+    let logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::default()))
+        .build(format!("log/{}.log", date)) // set the file name based on the current date
+        .unwrap();
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder().appender("logfile").build(LevelFilter::Info))
+        .unwrap();
+    log4rs::init_config(config).unwrap();
 }
