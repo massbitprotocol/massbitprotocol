363e98efc3b1e4ee60cee3e694b47ccf6b110b8e
thala321 <phanthanhhuy1996@gmail.com>
2021-08-17 17:17:32 +0700

    Restructure our tests and return encoded data so the UI doesn't have to call API to get that encoded data

--- user-example/create_example_json.py
+++ user-example/create_example_json.py
@@ -1,5 +1,8 @@
 import os
 import json
+import os.path
+from os import path
+import urllib.parse
 
 git_path = "https://raw.githubusercontent.com/massbitprotocol/massbitprotocol/main/"
 user_example_folder = "user-example"
@@ -18,33 +21,50 @@ def get_file(path):
     files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     # Remove hidden files
     files = [f for f in files if f[0] != "."]
+    print(path)
     return files
+
+
+def read_file_content(path):
+    file = open(path)
+    content = urllib.parse.quote_plus(file.read())
+    file.close()
+    return content
 
 
 if __name__ == '__main__':
     dir_json = {}
     chains = get_dirs(os.path.join('.'))
-    for chain in chains:
+    for chain in chains:  # substrate
         dir_json[chain] = {}
-        examples = get_dirs(os.path.join('.', chain))
-        for example in examples:
-            dir_json[chain][example] = {}
-            files = get_file(os.path.join('.', chain, example, "src"))
+        types = get_dirs(os.path.join('.', chain))
+        for type in types:  # substrate/so
+            dir_json[chain][type] = {}
+            chain_and_type = os.path.join('.', chain, type)
+            for example in get_dirs(chain_and_type):  # substrate/so/test-block
+                dir_json[chain][type][example] = {}
+                chain_and_type_and_example = os.path.join(chain_and_type, example)
 
-            # If there are files in `src` folder
-            if len(files) != 0:
-                for file in files:
-                    dir_json[chain][example][file] = git_path + os.path.join(user_example_folder, chain, example, "src",
-                                                                             file)
-            # If there are folders in `src` folder
-            else:
-                sub_src_dirs = get_dirs(os.path.join('.', chain, example, "src"))
-                for sub_src_dir in sub_src_dirs:
-                    dir_json[chain][example][sub_src_dir] = {}
-                    files = get_file(os.path.join('.', chain, example, "src", sub_src_dir))
-                    for file in files:
-                        dir_json[chain][example][file] = git_path + os.path.join(user_example_folder, chain, example,
-                                                                                 "src", sub_src_dir, file)
+                # Find folders (src, abis,...) in the example
+                for folder in get_dirs(chain_and_type_and_example):  # substrate/so/test-block/src or substrate/so/test-block/abis  ...
+                    dir_json[chain][type][example][folder] = {}
+                    dir_json[chain][type][example]["src"] = {}  # Set this up for quickswap
+                    chain_and_type_and_example_and_folder = os.path.join(chain_and_type_and_example, folder)
+
+                    # Go 1 deeper level to find the mappings of quickswap  # ethereum/wasm/quickswap/src/mappings/...
+                    if "quickswap" in chain_and_type_and_example_and_folder:
+                        quickswap_mappings_path = os.path.join(chain_and_type_and_example, "src", "mappings")
+                        for quickswap_file in get_file(quickswap_mappings_path):
+                            dir_json[chain][type][example]["src"]["mappings/" + quickswap_file] = read_file_content(os.path.join(quickswap_mappings_path, quickswap_file))
+
+                    for file in get_file(chain_and_type_and_example_and_folder):  # substrate/so/test-block/src/mapping.rs or substrate/so/test-block/abis/abi.json ....
+                        chain_and_type_and_example_and_folder_and_file = os.path.join(chain_and_type_and_example_and_folder, file)
+                        dir_json[chain][type][example][folder][file] = read_file_content(chain_and_type_and_example_and_folder_and_file)
+
+                # Find files (project.yaml, schema.graphql, package.json...) in the example
+                for file in get_file(chain_and_type_and_example):
+                    chain_and_type_and_example_and_file = os.path.join(chain_and_type_and_example, file)
+                    dir_json[chain][type][example][file] = read_file_content(chain_and_type_and_example_and_file)
 
     print(json.dumps(dir_json, indent=4, sort_keys=True))
     with open(result_file, 'w') as fp:
