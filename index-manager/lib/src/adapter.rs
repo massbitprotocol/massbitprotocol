fcbd7d90a8b5ff89a216870ee6147a4872de786f
thala321 <phanthanhhuy1996@gmail.com>
2021-08-24 19:46:58 +0700

    Start the handler based on mapping type

--- index-manager/lib/src/adapter.rs
+++ index-manager/lib/src/adapter.rs
@@ -2,19 +2,26 @@ use crate::ipfs::read_config_file;
 use crate::type_index::IndexConfig;
 use adapter::core::AdapterManager;
 use std::error::Error;
+use crate::config::get_mapping_language;
 
 pub async fn adapter_init(index_config: &IndexConfig) -> Result<(), Box<dyn Error>> {
     // Chain Reader Client Configuration to subscribe and get latest block from Chain Reader Server
     let config_value = read_config_file(&index_config.config);
-
     log::info!("Load library from {:?}", &index_config.mapping);
-    let mut adapter = AdapterManager::new();
-    adapter
-        .init(
-            &index_config.identifier.name_with_hash,
-            &config_value,
-            &index_config.mapping,
-        )
-        .await;
+
+    if get_mapping_language(&config_value).to_string().contains("wasm") {
+        log::info!("Handling .wasm file");
+        // TODO: we have the datasource, now the handler can get the ethereum event
+    } else {
+        log::info!("Handling .so file");
+        let mut adapter = AdapterManager::new();
+        adapter
+            .init(
+                &index_config.identifier.name_with_hash,
+                &config_value,
+                &index_config.mapping,
+            )
+            .await;
+    }
     Ok(())
 }
