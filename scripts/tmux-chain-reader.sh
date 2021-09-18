a40efabffdceda717f5741940057ba0d3f0872ca
anhhuy <anhhuy0501@gmail.com>
2021-09-18 10:50:58 +0700

    Saving log on console to file

--- scripts/tmux-chain-reader.sh
+++ scripts/tmux-chain-reader.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-RUST_LOG_TYPE=file RUST_LOG=debug cargo run --bin chain-reader
+RUST_LOG_TYPE=file RUST_LOG=debug cargo run --bin chain-reader 2>&1 | tee log/console-chain-reader.log
--- scripts/tmux-index-manager.sh
+++ scripts/tmux-index-manager.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-RUST_LOG_TYPE=file cargo run --bin index-manager-main
+RUST_LOG_TYPE=file cargo run --bin index-manager-main 2>&1 | tee log/console-index-manager.log
