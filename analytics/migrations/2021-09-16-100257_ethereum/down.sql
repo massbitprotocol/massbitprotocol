d7978058aa8a628fb2573e7f643e8b4f91bd1816
Viet Tai <vuviettai@gmail.com>
2021-09-22 17:58:59 +0700

    Add migrations directory

---
+++ analytics/migrations/2021-09-16-100257_ethereum/down.sql
@@ -0,0 +1,5 @@
+-- This file should undo anything in `up.sql`
+drop table if exists matic_block;
+drop table if exists matic_transaction;
+drop table if exists matic_daily_transaction;
+drop table if exists matic_daily_address_transaction;
