4730e0d4f880190d028412cfc6ad48746561af1f
Viet Tai <vuviettai@gmail.com>
2021-10-14 17:45:26 +0700

    Add 18 tables for parser spltoken

--- analytics/migrations/2021-10-13-143000_solana_token/up.sql
+++ analytics/migrations/2021-10-13-143000_solana_token/up.sql
@@ -57,11 +57,24 @@ create table solana_spl_token_transfer
     source              varchar(88),
     destination         varchar(88),
     amount              bigint,
-    signers             text,
     authority           varchar(88),
-    multisig_authority  varchar(88)
+    multisig_authority  varchar(88),
+    signers             text
 );
-
+create table solana_spl_token_transfer_checked
+(
+    block_slot                  bigint,
+    block_time                  bigint,
+    tx_index                    int,    --Index of transaction in block
+    instruction_index           int,    --Index of instruction in transaction
+    source                      varchar(88),
+    mint                        varchar(88),
+    destination                 varchar(88),
+    token_amount                varchar(88),
+    authority                   varchar(88),
+    multisig_fauthority         text,
+    signers                     text
+);
 create table solana_spl_token_approve
 (
     block_slot          bigint,
@@ -71,7 +84,152 @@ create table solana_spl_token_approve
     source              varchar(88),
     delegate            varchar(88),
     amount              bigint,
+    owner               varchar(88),
+    multisig_owner      text,
+    signers             text
+);
+
+
+create table solana_spl_token_approve_checked
+(
+    block_slot                  bigint,
+    block_time                  bigint,
+    tx_index                    int,    --Index of transaction in block
+    instruction_index           int,    --Index of instruction in transaction
+    source                      varchar(88),
+    mint                        varchar(88),
+    delegate                    varchar(88),
+    token_amount                varchar(88),
+    owner                       varchar(88),
+    multisig_owner              text,
+    signers                     text
+);
+
+create table solana_spl_token_revoke
+(
+    block_slot          bigint,
+    block_time          bigint,
+    tx_index            int,    --Index of transaction in block
+    instruction_index   int,    --Index of instruction in transaction
+    source              varchar(88),
     signers             text,
     owner               varchar(88),
     multisig_owner      text
+);
+
+create table solana_spl_token_set_authority
+(
+    block_slot          bigint,
+    block_time          bigint,
+    tx_index            int,    --Index of transaction in block
+    instruction_index   int,    --Index of instruction in transaction
+    authority_type      varchar(88),
+    new_authority       varchar(88),
+    signers             text,
+    authority           varchar(88),
+    multisig_authority  text
+);
+
+create table solana_spl_token_mint_to
+(
+    block_slot          bigint,
+    block_time          bigint,
+    tx_index            int,    --Index of transaction in block
+    instruction_index   int,    --Index of instruction in transaction
+    mint                varchar(88),
+    account             varchar(88),
+    amount              varchar(88),
+    mint_authority      varchar(88),
+    multisig_mint_authority  text,
+    signers             text
+);
+
+create table solana_spl_token_min_to_checked
+(
+    block_slot                  bigint,
+    block_time                  bigint,
+    tx_index                    int,    --Index of transaction in block
+    instruction_index           int,    --Index of instruction in transaction
+    mint                        varchar(88),
+    account                     varchar(88),
+    token_amount                varchar(88),
+    mint_authority              varchar(88),
+    multisig_mint_authority     varchar(88),
+    signers                     text
+);
+
+create table solana_spl_token_burn
+(
+    block_slot          bigint,
+    block_time          bigint,
+    tx_index            int,    --Index of transaction in block
+    instruction_index   int,    --Index of instruction in transaction
+    account             varchar(88),
+    mint                varchar(88),
+    amount              varchar(88),
+    signers             text,
+    authority      varchar(88),
+    multisig_authority  text
+);
+
+create table solana_spl_token_burn_checked
+(
+    block_slot                  bigint,
+    block_time                  bigint,
+    tx_index                    int,    --Index of transaction in block
+    instruction_index           int,    --Index of instruction in transaction
+    account                     varchar(88),
+    mint                        varchar(88),
+    token_amount                varchar(88),
+    authority              varchar(88),
+    multisig_mint_authority     varchar(88),
+    signers                     text
+);
+
+create table solana_spl_token_close_account
+(
+    block_slot          bigint,
+    block_time          bigint,
+    tx_index            int,    --Index of transaction in block
+    instruction_index   int,    --Index of instruction in transaction
+    account             varchar(88),
+    destination         varchar(88),
+    owner               varchar(88),
+    signers             text,
+    multisig_owner      text
+);
+
+create table solana_spl_token_freeze_account
+(
+    block_slot                  bigint,
+    block_time                  bigint,
+    tx_index                    int,    --Index of transaction in block
+    instruction_index           int,    --Index of instruction in transaction
+    account                     varchar(88),
+    mint                        varchar(88),
+    freeze_authority            varchar(88),
+    signers                     text,
+    multisig_freeze_authority   text
+);
+
+create table solana_spl_token_thaw_account
+(
+    block_slot                  bigint,
+    block_time                  bigint,
+    tx_index                    int,    --Index of transaction in block
+    instruction_index           int,    --Index of instruction in transaction
+    account                     varchar(88),
+    mint                        varchar(88),
+    freeze_authority            varchar(88),
+    signers                     text,
+    multisig_freeze_authority   text
+);
+
+create table solana_spl_token_sync_native
+(
+    block_slot                  bigint,
+    block_time                  bigint,
+    tx_index                    int,    --Index of transaction in block
+    instruction_index           int,    --Index of instruction in transaction
+    account                     varchar(88)
 );
