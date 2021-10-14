679c4a7ecc455192e571500923ebbd0841f95c8f
Viet Tai <vuviettai@gmail.com>
2021-10-14 11:31:01 +0700

    Ignore hasura call error

--- core/index-store/src/postgres/store_builder.rs
+++ core/index-store/src/postgres/store_builder.rs
@@ -1,17 +1,7 @@
-use graph_mock::MockMetricsRegistry;
-use graph_store_postgres::{
-    command_support::{catalog::Site, Namespace},
-    connection_pool::ConnectionPool,
-    relational::Layout,
-    PRIMARY_SHARD,
-};
-use massbit_common::prelude::{
-    anyhow::{self, anyhow},
-    slog::{self, Logger},
-};
-use std::sync::Arc;
 use super::relational::LayoutExt;
 use super::PostgresIndexStore;
+use crate::models::Indexer;
+use crate::schema::indexers;
 use diesel::prelude::*;
 use diesel::sql_types::BigInt;
 use diesel::QueryableByName;
@@ -20,12 +10,19 @@ use graph::cheap_clone::CheapClone;
 use graph::data::schema::Schema;
 use graph::log::logger;
 use graph::prelude::{DeploymentHash, NodeId, StoreError};
+use graph_mock::MockMetricsRegistry;
 use graph_node::{
     config::{Config, Opt},
     store_builder::StoreBuilder as GraphStoreBuilder,
 };
 use graph_store_postgres::command_support::Catalog;
 use graph_store_postgres::primary::DeploymentId;
+use graph_store_postgres::{
+    command_support::{catalog::Site, Namespace},
+    connection_pool::ConnectionPool,
+    relational::Layout,
+    PRIMARY_SHARD,
+};
 use massbit_common::consts::HASURA_URL;
 use massbit_common::prelude::diesel::connection::SimpleConnection;
 use massbit_common::prelude::diesel::{sql_query, RunQueryDsl};
@@ -34,12 +31,15 @@ use massbit_common::prelude::log::{self, error};
 use massbit_common::prelude::reqwest::Client;
 use massbit_common::prelude::serde_json;
 use massbit_common::prelude::tokio_compat_02::FutureExt;
+use massbit_common::prelude::{
+    anyhow::{self, anyhow},
+    slog::{self, Logger},
+};
 use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use crate::schema::indexers;
-use crate::models::Indexer;
+use std::sync::Arc;
 lazy_static! {
     pub static ref GRAPH_NODE: NodeId = NodeId::new("graph_node").unwrap();
     //pub static ref NAMESPACE: Namespace = Namespace::new("sgd0".to_string()).unwrap();
@@ -52,9 +52,13 @@ const CONN_POOL_SIZE: u32 = 20;
 
 pub struct StoreBuilder {}
 impl StoreBuilder {
-    pub fn prepare_schema(indexer_hash: &str, conn: &PgConnection) -> Result<String, anyhow::Error>  {
+    pub fn prepare_schema(
+        indexer_hash: &str,
+        conn: &PgConnection,
+    ) -> Result<String, anyhow::Error> {
         log::info!("Prepare schema for indexer {}", indexer_hash);
-        let entity = indexers::table.filter(indexers::id.eq(indexer_hash))
+        let entity = indexers::table
+            .filter(indexers::id.eq(indexer_hash))
             .limit(1)
             .load::<Indexer>(conn)
             .expect("Error loading indexer state")
@@ -65,10 +69,10 @@ impl StoreBuilder {
             "SELECT count(schema_name) FROM information_schema.schemata WHERE schema_name = '{}'",
             entity.namespace.as_str()
         ))
-            .get_results::<Counter>(conn)
-            .expect("Query failed")
-            .pop()
-            .expect("No record found");
+        .get_results::<Counter>(conn)
+        .expect("Query failed")
+        .pop()
+        .expect("No record found");
         if counter.count == 0 {
             //Create schema
             match sql_query(format!("create schema {}", entity.namespace.as_str())).execute(conn) {
@@ -119,13 +123,13 @@ impl StoreBuilder {
                         Ok(PostgresIndexStore {
                             connection,
                             layout,
-                            logger
+                            logger,
                         })
                     }
                     Err(e) => Err(e.into()),
                 }
             }
-            Err(err) => Err(err)
+            Err(err) => Err(err),
         }
     }
     pub fn create_relational_schema<P: AsRef<Path>>(
@@ -199,8 +203,7 @@ impl StoreBuilder {
                         .json(&payload)
                         .send()
                         .compat()
-                        .await
-                        .unwrap();
+                        .await;
                     log::info!("Hasura {:?}", response);
                 });
                 Ok(layout)
