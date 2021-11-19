84abb8666066835b26b2d8ae53f3af2e1e475d9a
anhhuy <anhhuy0501@gmail.com>
2021-11-19 14:52:26 +0700

    Auto stash before checking out "HEAD"

--- chain-reader/src/solana_chain.rs
+++ chain-reader/src/solana_chain.rs
@@ -17,7 +17,11 @@ use solana_client::rpc_client::RpcClient;
 use solana_client::rpc_request::RpcRequest;
 use solana_client::rpc_response::RpcConfirmedTransactionStatusWithSignature;
 
+use massbit::prelude::lazy_static;
+// use massbit::slog::MutexDrainError::Mutex;
 use massbit_common::prelude::diesel::serialize::IsNull::No;
+//use solana_runtime::contains::Contains;
+use massbit_common::prelude::diesel::RunQueryDsl;
 use solana_sdk::account::Account;
 use solana_sdk::clock::Slot;
 use solana_sdk::signature::Signature;
@@ -28,7 +32,12 @@ use solana_transaction_status::{
 use std::collections::HashMap;
 use std::error::Error;
 use std::str::FromStr;
-use std::{sync::Arc, time::Instant};
+use std::sync::atomic::Ordering;
+use std::sync::atomic::{AtomicU64, AtomicUsize};
+use std::{
+    sync::{Arc, Mutex},
+    time::Instant,
+};
 use tokio::sync::mpsc;
 use tonic::Status;
 
@@ -37,12 +46,20 @@ const CHAIN_TYPE: ChainType = ChainType::Solana;
 const VERSION: &str = "1.7.0";
 const BLOCK_AVAILABLE_MARGIN: u64 = 100;
 const RPC_BLOCK_ENCODING: UiTransactionEncoding = UiTransactionEncoding::Base64;
-const GET_BLOCK_TIMEOUT_SEC: u64 = 60;
-const BLOCK_BATCH_SIZE: u64 = 10;
+const GET_BLOCK_TIMEOUT_SEC: u64 = 120;
+const BLOCK_BATCH_SIZE: u64 = 2;
+//massbit 2: 1-> 5,695ms, 10->7.54s, 50 -> 14.2ms, 100 -> 17.8ms
+//massbit 3: 20-> ,50 -> 8.983ms, 100 -> 10.289ms
 const GET_NEW_SLOT_DELAY_MS: u64 = 500;
 const TRANSACTION_BATCH_SIZE: usize = 100;
 // The max value is 1000
 const LIMIT_FILTER_RESULT: usize = 1000;
+
+lazy_static! {
+    pub static ref BLOCK_COUNT: AtomicUsize = AtomicUsize::new(0);
+    pub static ref GET_BLOCK_DURATION_MS: AtomicUsize = AtomicUsize::new(0);
+    pub static ref BLOCK_TIMEOUT_COUNT: AtomicUsize = AtomicUsize::new(0);
+}
 
 #[derive(Debug)]
 struct ResultFilterTransaction {
@@ -138,7 +155,7 @@ fn getFilterConfirmedTransactionStatus(
 async fn grpc_send_block(
     block: BlockResponse,
     chan: &mpsc::Sender<Result<BlockResponse, Status>>,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<(), Box<dyn Error + Send + Sync>> {
     if !chan.is_closed() {
         chan.send(Ok(block)).await;
     } else {
@@ -147,17 +164,77 @@ async fn grpc_send_block(
     Ok(())
 }
 
+fn decode_and_filter(
+    filter: Arc<SolanaFilter>,
+    block: EncodedConfirmedBlock,
+) -> Option<solana_transaction_status::ConfirmedBlock> {
+    let decode_block = decode_encoded_block(block);
+    let filtered_block = filter.filter_block(decode_block);
+    if filtered_block.transactions.is_empty() {
+        println!(
+            "Block slot {} has no match Transaction",
+            &filtered_block.parent_slot + 1
+        );
+        None
+    } else {
+        Some(filtered_block)
+    }
+}
+#[derive(Clone, Debug, Default)]
+struct SendQueue {
+    queue: HashMap<Slot, Option<ConfirmedBlock>>,
+    sending_slot: u64,
+}
+
+async fn insert_and_prepare_send(
+    send_queue: Arc<Mutex<SendQueue>>,
+    block: Option<ConfirmedBlock>,
+    block_slot: &u64,
+) -> Option<BlockResponse> {
+    let mut send_queue = send_queue.lock().unwrap();
+    let mut count_send_block = 0;
+    // Insert block into queue
+    send_queue.queue.insert(*block_slot, block);
+    // Check if there are blocks that could be sending
+    let mut end_slot: Option<u64> = None;
+    let mut check_slot = send_queue.sending_slot;
+    let mut blocks = vec![];
+    loop {
+        if send_queue.queue.contains_key(&check_slot) {
+            let block = send_queue.queue.remove(&check_slot);
+            if let Some(Some(block)) = block {
+                blocks.push((block, check_slot));
+            }
+            check_slot = check_slot + 1;
+        } else {
+            send_queue.sending_slot = check_slot;
+            break;
+        }
+    }
+
+    drop(send_queue);
+    if count_send_block != 0 {
+        let block_response = _to_generic_block(blocks);
+        info!("Packed {} blocks to send", count_send_block);
+        Some(block_response)
+    } else {
+        None
+    }
+}
+
 pub async fn loop_get_block(
     chan: mpsc::Sender<Result<BlockResponse, Status>>,
     start_block: &Option<u64>,
     network: &NetworkType,
     client: &Arc<RpcClient>,
     filter: &SolanaFilter,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<(), Box<dyn Error + Send + Sync>> {
     info!(
         "Start get block Solana from: {:?} with filter {:?}",
         start_block, filter
     );
+    let filter = Arc::new(filter.clone());
+
     let _config = CONFIG.get_chain_config(&CHAIN_TYPE, &network).unwrap();
     // let websocket_url = config.ws.clone();
     // let (mut _subscription_client, receiver) =
@@ -169,7 +246,8 @@ pub async fn loop_get_block(
     let mut before_tx_signature = None;
 
     let mut filter_txs = vec![];
-    // Backward run
+
+    //******************* Backward check ***************************//
     info!(
         "Start get transaction backward with filter address: {:?}",
         &filter
@@ -195,8 +273,9 @@ pub async fn loop_get_block(
             }
         }
     }
+    //******************* Forward run ***************************//
     info!("Start get {} transaction forward.", filter_txs.len());
-    // Forward run
+
     let mut start_tx: usize = filter_txs.len();
     while start_tx > 0 {
         let transactions = getTransactions(client, &filter_txs, &mut start_tx);
@@ -262,13 +341,37 @@ pub async fn loop_get_block(
     }
 
     // start from the last indexed block
-    let mut last_indexed_slot: Option<u64> = filter_txs.last().map(|tx| tx.slot);
+    let mut last_indexed_slot: u64 = match filter_txs.last().map(|tx| tx.slot) {
+        Some(last_indexed_slot) => last_indexed_slot,
+        None => {
+            // let get_slot = || {
+            //     client.get_slot().unwrap_or_else(get_slot)
+            // };
+            // get_slot()
+            let mut last_indexed_slot = 0;
+            loop {
+                match client.get_slot() {
+                    Ok(_last_indexed_slot) => {
+                        last_indexed_slot = _last_indexed_slot - BLOCK_AVAILABLE_MARGIN;
+                        break;
+                    }
+                    Err(_) => continue,
+                }
+            }
+            last_indexed_slot
+        }
+    };
 
     info!(
         "Start getting forward at the block {:?} .",
         &last_indexed_slot
     );
-    // from current block run
+    //******************* From current block run ***************************//
+    // Last sending block number
+    let send_queue: Arc<Mutex<SendQueue>> = Arc::new(Mutex::new(SendQueue {
+        queue: HashMap::new(),
+        sending_slot: last_indexed_slot,
+    }));
     loop {
         if chan.is_closed() {
             return Err("Stream is closed!".into());
@@ -278,76 +381,66 @@ pub async fn loop_get_block(
                 // Root is finalized block in Solana
                 let current_root = new_slot - BLOCK_AVAILABLE_MARGIN;
                 //info!("Root: {:?}",new_info.root);
-                match last_indexed_slot {
-                    Some(value_last_indexed_slot) => {
-                        if current_root == value_last_indexed_slot {
-                            sleep(Duration::from_millis(GET_NEW_SLOT_DELAY_MS)).await;
-                            continue;
-                        }
-                        info!(
-                            "Latest stable block: {}, Pending block: {}",
-                            current_root,
-                            current_root - value_last_indexed_slot
-                        );
-                        let mut tasks = vec![];
-                        let number_get_slot =
-                            (current_root - value_last_indexed_slot).min(BLOCK_BATCH_SIZE);
-                        let block_range =
-                            value_last_indexed_slot..(value_last_indexed_slot + number_get_slot);
+                if current_root == last_indexed_slot {
+                    sleep(Duration::from_millis(GET_NEW_SLOT_DELAY_MS)).await;
+                    continue;
+                }
+                info!(
+                    "Latest stable block: {}, Pending block: {}",
+                    current_root,
+                    current_root - last_indexed_slot
+                );
+                // let mut tasks = vec![];
+                let number_get_slot = (current_root - last_indexed_slot).min(BLOCK_BATCH_SIZE);
+                let block_range = last_indexed_slot..(last_indexed_slot + number_get_slot);
 
-                        for block_slot in block_range {
-                            let new_client = client.clone();
-                            let permit = Arc::clone(&sem).acquire_owned().await.unwrap();
-                            tasks.push(tokio::spawn(async move {
-                                let res = timeout(
-                                    Duration::from_secs(GET_BLOCK_TIMEOUT_SEC),
-                                    get_block(new_client, permit, block_slot),
+                for block_slot in block_range {
+                    let new_client = client.clone();
+                    let chan_clone = chan.clone();
+                    let filter_clone = filter.clone();
+                    let permit = Arc::clone(&sem).acquire_owned().await.unwrap();
+                    let mut send_queue_clone = send_queue.clone();
+                    tokio::spawn(async move {
+                        let res = timeout(
+                            Duration::from_secs(GET_BLOCK_TIMEOUT_SEC),
+                            get_block(new_client, permit, block_slot),
+                        )
+                        .await;
+                        info!(
+                            "Finish tokio::spawn for getting block number: {:?}",
+                            &block_slot
+                        );
+
+                        match res {
+                            Ok(Ok((block, slot))) => {
+                                let decoded_block = decode_and_filter(filter_clone, block);
+                                let block_response = insert_and_prepare_send(
+                                    send_queue_clone,
+                                    decoded_block,
+                                    &block_slot,
                                 )
                                 .await;
-                                if res.is_err() {
-                                    warn!("get_block timed out at block number {}", &block_slot);
-                                };
-                                info!(
-                                    "Finish tokio::spawn for getting block number: {:?}",
-                                    &block_slot
-                                );
-                                res.unwrap()
-                            }));
-                        }
-                        let blocks_with_number: Vec<Result<_, _>> =
-                            futures03::future::join_all(tasks).await;
-                        let mut blocks_with_number: Vec<(ConfirmedBlock, u64)> = blocks_with_number
-                            .into_iter()
-                            .filter_map(|res_block| {
-                                if let Ok(Ok((block, block_number))) = res_block {
-                                    info!("Got SOLANA block at slot : {:?}", &block_number);
-                                    let decode_block = decode_encoded_block(block);
-                                    let filtered_block = filter.filter_block(decode_block);
-                                    if filtered_block.transactions.is_empty() {
-                                        println!(
-                                            "Block slot {} has no match Transaction",
-                                            &block_number
-                                        );
-                                        None
-                                    } else {
-                                        Some((filtered_block, block_number))
-                                    }
-                                } else {
-                                    None
+                                // if let Some(block_response) = block_response {
+                                //     grpc_send_block(block_response, &chan_clone).await;
+                                //     info!("Send block_response to indexer-manager");
+                                // }
+                            }
+                            Err(_) | Ok(Err(_)) => {
+                                warn!("get_block timed out at block number {}", &block_slot);
+                                BLOCK_TIMEOUT_COUNT.fetch_add(1, Ordering::SeqCst);
+                                let block_response =
+                                    insert_and_prepare_send(send_queue_clone, None, &block_slot)
+                                        .await;
+                                if let Some(block_response) = block_response {
+                                    grpc_send_block(block_response, &chan_clone).await;
+                                    info!("Send block_response to indexer-manager");
                                 }
-                            })
-                            .collect();
-                        blocks_with_number.sort_by(|a, b| a.1.cmp(&b.1));
-                        info!("Finished get {} filtered blocks", blocks_with_number.len());
-                        if !blocks_with_number.is_empty() {
-                            let block_response = _to_generic_block(blocks_with_number);
-                            grpc_send_block(block_response, &chan).await?;
+                            }
                         }
-                        last_indexed_slot = last_indexed_slot
-                            .map(|last_indexed_slot| last_indexed_slot + number_get_slot);
-                    }
-                    _ => last_indexed_slot = Some(current_root),
-                };
+                    });
+                }
+
+                last_indexed_slot = last_indexed_slot + number_get_slot;
             }
             Err(err) => {
                 eprintln!("Get slot error: {:?}", err);
@@ -367,9 +460,7 @@ fn _create_generic_block(blocks: &Vec<Block>) -> BlockResponse {
     generic_data
 }
 
-fn _to_generic_block(
-    blocks_with_number: Vec<(solana_transaction_status::ConfirmedBlock, u64)>,
-) -> BlockResponse {
+fn _to_generic_block(blocks_with_number: Vec<(ConfirmedBlock, u64)>) -> BlockResponse {
     let ext_blocks: Vec<Block> = blocks_with_number
         .into_iter()
         .map(|(block, block_number)| {
@@ -399,18 +490,24 @@ async fn get_block(
     let now = Instant::now();
     let block = client.get_block_with_encoding(block_number, RPC_BLOCK_ENCODING);
     let elapsed = now.elapsed();
+    let duration = GET_BLOCK_DURATION_MS.fetch_add(elapsed.as_millis() as usize, Ordering::SeqCst)
+        + elapsed.as_millis() as usize;
+    let block_count = BLOCK_COUNT.fetch_add(1, Ordering::SeqCst) + 1;
     match block {
         Ok(block) => {
             info!(
-                "Finished RPC get Block: {:?}, time: {:?}, hash: {}",
-                block_number, elapsed, &block.blockhash
+                "Finished RPC get Block: {:?}, get-block duration: {:?}, average get-block duration: {} ms, timeout ratio: {}, hash: {}",
+                block_number, elapsed,(duration/block_count), (BLOCK_TIMEOUT_COUNT.load(Ordering::SeqCst) as f32 / block_number as f32), &block.blockhash
             );
             Ok((block, block_number))
         }
-        _ => {
+        Err(e) => {
+            if format!("{:?}", &e).contains("TimedOut") {
+                BLOCK_TIMEOUT_COUNT.fetch_add(1, Ordering::SeqCst);
+            }
             info!(
-                "Cannot get RPC get Block: {:?}, Error:{:?}, time: {:?}",
-                block_number, block, elapsed
+                "Cannot get RPC get Block: {:?}, Error:{:?}, get-block duration: {:?}, average get-block duration: {} ms, timeout ratio: {}",
+                block_number, &e, elapsed, (duration/block_count),(BLOCK_TIMEOUT_COUNT.load(Ordering::SeqCst) as f32 / block_number as f32)
             );
             Err(format!("Error cannot get block").into())
         }
