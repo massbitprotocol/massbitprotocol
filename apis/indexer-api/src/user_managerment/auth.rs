71fb56ca4067d17a861e9a95ffcfbd91107db272
anhhuy0501 <anhhuy0501@gmail.com>
2021-12-29 16:23:36 +0700

    211228 user managerment (#431)
    * Add user managerment + add my-indexer list api
    
    * Change authorization algorithm to rs256
    
    * fix missing pubkey.pem for workflow testing
    
    * remove unused function

---
+++ apis/indexer-api/src/user_managerment/auth.rs
@@ -0,0 +1,91 @@
+use crate::user_managerment::{error::Error, Result, WebResult};
+use chrono::prelude::*;
+use hex;
+use jsonwebtoken::{
+    decode, decode_header, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation,
+};
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::fmt;
+use warp::{
+    filters::header::headers_cloned,
+    http::header::{HeaderMap, HeaderValue, AUTHORIZATION},
+    reject, Filter, Rejection,
+};
+
+lazy_static! {
+    pub static ref BEARER: String = String::from("Bearer ");
+    pub static ref JWT_SECRET_KEY: String = env::var("JWT_SECRET_KEY").unwrap_or(String::from(
+        "1F9853B9E546038E0736CB32C97085E07868552078A7745FBA3C5C78889F4CD2"
+    ));
+}
+#[derive(Clone, PartialEq)]
+pub enum Role {
+    User,
+    //Admin,
+}
+
+impl Role {
+    pub fn from_str(role: &str) -> Role {
+        match role {
+            //"Admin" => Role::Admin,
+            _ => Role::User,
+        }
+    }
+}
+
+impl fmt::Display for Role {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Role::User => write!(f, "User"),
+            //Role::Admin => write!(f, "Admin"),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Claims {
+    sub: String,
+    exp: usize,
+    iat: usize,
+}
+
+pub fn with_auth(role: Role) -> impl Filter<Extract = (String,), Error = Rejection> + Clone {
+    headers_cloned()
+        .map(move |headers: HeaderMap<HeaderValue>| (role.clone(), headers))
+        .and_then(authorize)
+}
+
+async fn authorize((role, headers): (Role, HeaderMap<HeaderValue>)) -> WebResult<String> {
+    match jwt_from_header(&headers) {
+        Ok(jwt) => {
+            let decoded = decode::<Claims>(
+                &jwt,
+                &DecodingKey::from_rsa_pem(include_bytes!("pubkey.pem"))
+                    .expect("Invalid pubkey in pubkey.pem"),
+                &Validation::new(Algorithm::RS256),
+            )
+            .map_err(|_| reject::custom(Error::JWTTokenError))?;
+
+            log::info!("authorize: {:?}", decoded.claims);
+            Ok(decoded.claims.sub)
+        }
+        Err(e) => return Err(reject::custom(e)),
+    }
+}
+
+fn jwt_from_header(headers: &HeaderMap<HeaderValue>) -> Result<String> {
+    let header = match headers.get(AUTHORIZATION) {
+        Some(v) => v,
+        None => return Err(Error::NoAuthHeaderError),
+    };
+    let auth_header = match std::str::from_utf8(header.as_bytes()) {
+        Ok(v) => v,
+        Err(_) => return Err(Error::NoAuthHeaderError),
+    };
+    if !auth_header.starts_with(BEARER.as_str()) {
+        return Err(Error::InvalidAuthHeaderError);
+    }
+    Ok(auth_header.trim_start_matches(BEARER.as_str()).to_owned())
+}
--- scripts/remote_deploy/deploy-chain-reader.sh
+++ scripts/remote_deploy/deploy-chain-reader.sh
@@ -1,3 +1,3 @@
 tmux list-sessions | awk 'BEGIN{FS=":"}{print $1}' | xargs -n 1 tmux kill-session -t
 cd massbitprotocol/deployment/binary
-tmux new-session -d -s "chain-reader" "RUST_LOG=debug ./chain-reader 2>&1 | tee log/console-chain-reader.log"
+tmux new-session -d -s "chain-reader" "./chain-reader 2>&1 | tee log/console-chain-reader.log"
