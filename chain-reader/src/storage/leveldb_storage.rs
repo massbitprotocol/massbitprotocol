683b4445cf8d1bfed53e2f61ee06c9b53330f516
Viet Tai <vuviettai@gmail.com>
2021-12-30 12:12:43 +0700

    [WIP]

--- chain-reader/src/storage/leveldb_storage.rs
+++ chain-reader/src/storage/leveldb_storage.rs
@@ -1,21 +1,47 @@
 use crate::storage::BlockStorage;
 use std::path::Path;
+extern crate db_key as key;
 extern crate leveldb;
-
+use chain_ethereum::MappingTrigger::Block;
+use key::Key;
 use leveldb::database::Database;
+use leveldb::error::Error;
 use leveldb::iterator::Iterable;
 use leveldb::kv::KV;
 use leveldb::options::{Options, ReadOptions, WriteOptions};
+use log::log;
+use solana_program::clock::Slot;
 
 pub struct BlockKey {
-    
+    slot: u64,
 }
 
-impl Key fo {
-    
+impl Key for BlockKey {
+    fn from_u8(key: &[u8]) -> Self {
+        assert!(key.len() == 8);
+        let mut slot: u64 = key[0] as u64;
+        for i in 1..8 {
+            slot = slot << 8 | key[i] as u64;
+        }
+        BlockKey { slot }
+    }
+
+    fn as_slice<T, F: Fn(&[u8]) -> T>(&self, f: F) -> T {
+        let mut dst = [0u8, 0, 0, 0, 0, 0, 0, 0];
+        for i in 0..8 {
+            dst[i] = (self.slot >> (8 * (7 - i))) as u8;
+        }
+        f(&dst)
+    }
+}
+
+impl From<Slot> for BlockKey {
+    fn from(slot: Slot) -> Self {
+        BlockKey { slot }
+    }
 }
 pub struct LevelDBStorage {
-    database: Database<ii3232>,
+    database: Database<BlockKey>,
 }
 
 impl LevelDBStorage {
@@ -33,11 +59,32 @@ impl LevelDBStorage {
     }
 }
 impl BlockStorage for LevelDBStorage {
-    fn store_block(&self, block_slot: , content: &[u8]) {
-        todo!()
+    fn store_block(&self, block_slot: Slot, content: &[u8]) {
+        let write_opts = WriteOptions::new();
+        match self
+            .database
+            .put(write_opts, BlockKey::from(block_slot), content)
+        {
+            Ok(_) => {}
+            Err(err) => {
+                log::error!("failed to write to database: {:?}", &err);
+            }
+        };
+        self.database.keys_iter()
     }
 
-    fn get_block(&self, block_slot: u64) -> Vec<u8> {
-        todo!()
+    fn get_block(&self, block_slot: Slot) -> Option<Vec<u8>> {
+        let read_opts = ReadOptions::new();
+        match self.database.get(read_opts, BlockKey::from(block_slot)) {
+            Ok(res) => res,
+            Err(err) => {
+                log::error!(
+                    "failed to get block {:?} from database: {:?}",
+                    block_slot,
+                    &err
+                );
+                None
+            }
+        }
     }
 }
--- chain-reader/src/storage/mod.rs
+++ chain-reader/src/storage/mod.rs
@@ -2,5 +2,5 @@ mod leveldb_storage;
 
 pub trait BlockStorage {
     fn store_block(&self, block_slot: u64, content: &[u8]);
-    fn get_block(&self, block_slot: u64) -> Vec<u8>;
+    fn get_block(&self, block_slot: u64) -> Option<Vec<u8>>;
 }
